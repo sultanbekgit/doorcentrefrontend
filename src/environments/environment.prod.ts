ca4e5d9b1afba0b0c6edee1e9546f5766564d548
sultanbekgit <mgsultanbek04@gmail.com>
2025-08-02 22:30:56 +0500

    My commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://doorcenterbackend.onrender.com'
+  apiUrl: 'https://doorcenterrep.onrender.com'
 };
   
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,8 +1,7 @@
 export const environment = {
   production: false,
-  apiUrl: 'https://doorcenterbackend.onrender.com'
+  apiUrl: 'https://doorcenterrep.onrender.com'
 
-  
 
 };
   
